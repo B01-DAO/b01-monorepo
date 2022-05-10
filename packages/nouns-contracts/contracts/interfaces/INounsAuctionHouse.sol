75f17f134fe60b3b543f79b325e5b4831b5dd738
thebuidler <thebuidler@pm.me>
2022-05-09 20:42:38 -0500

    Added contract support for updating auction duration

--- packages/nouns-contracts/contracts/interfaces/INounsAuctionHouse.sol
+++ packages/nouns-contracts/contracts/interfaces/INounsAuctionHouse.sol
@@ -34,6 +34,8 @@ interface INounsAuctionHouse {
 
     event AuctionMinBidIncrementPercentageUpdated(uint256 minBidIncrementPercentage);
 
+    event AuctionDurationUpdated(uint256 duration);
+
     function settleAuction() external;
 
     function settleCurrentAndCreateNewAuction() external;
@@ -49,4 +51,6 @@ interface INounsAuctionHouse {
     function setReservePrice(uint256 reservePrice) external;
 
     function setMinBidIncrementPercentage(uint8 minBidIncrementPercentage) external;
+
+    function setDuration(uint256 duration) external;
 }
